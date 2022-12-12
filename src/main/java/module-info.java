498e886bd3cc695bbab0e7cd9dfbe575f2495676
Bilal-Naeem12 <fa21-bcs-029@cuilahore.edu.pk>
2022-12-12 23:14:56 +0500

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.semsterproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.semsterproject to javafx.fxml;
+    exports com.example.semsterproject;
+}
